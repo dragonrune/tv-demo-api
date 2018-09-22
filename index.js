9eb6ef069341905dbbc070eced6926754dbbec8c
Ken Mason <ken.mason52@yahoo.com>
2018-09-22 14:28:39 -0600

    TV Demo Api - Part 11

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require('express')
+const express = require ('express')
 const bodyParser = require ('body-parser')
 const app = express()
-const port = 3000
+const port = 3030
 
 app.use(bodyParser.json())
 
-app.get('/shows', (req, res) => res.send('App Get -- You have added a TV Show'))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+   })
+
+let TVShowArr=[]
+
+app.get('/shows', (req, res) => res.send(TVShowArr))
 
 app.post('/shows', (req, res) => {
+    TVShowArr.push(req.body)
     console.log(req.body)
-    res.send(`App Post -- ${req.body.TVShow}`)
+    res.send(TVShowArr)
 })
 
 app.put('/shows', (req, res) => {
